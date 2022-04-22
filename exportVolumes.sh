38353b32a6d26109296c08f57b10f1ba01d93e3d
juanghAdisic <juan.gomez@adisic.com>
2022-04-23 00:50:30 +0200

    v0.0.1

---
+++ exportVolumes.sh
@@ -0,0 +1,8 @@
+dir=${pwd}/backup
+
+if [[ ! -d $dir]]; then
+    mkdir -p $dir
+fi
+
+docker run --rm --volumes-from drupal -v ${dir}:/backup ubuntu bash -c "tar cvf /backup/drupalBackup.tar /var/www/html/modules /var/www/html/profiles /var/www/html/themes /var/www/html/sites"
+docker run --rm --volumes-from drupal_database -v ${dir}:/backup ubuntu bash -c "tar cvf /backup/mysqlBackup.tar /var/lib/mysql"
